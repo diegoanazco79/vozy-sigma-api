87a129d9371935b1cc29980e6943ad4248943529
diegoanazco79 <djanazcob@gmail.com>
2023-05-16 11:40:01 -0500

    :fire: DELETE CODE :: Delete a dotevn importation

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
-import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
 import { router as embedRouter } from "./routes/embeb";
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 const app = express();
 app.use(cors());
