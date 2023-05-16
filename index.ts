afe6fc13e83ded661b1771abc2a6069188d3f742
diegoanazco79 <djanazcob@gmail.com>
2023-05-16 10:23:02 -0500

    :sparkles: NEW FEATURE :: Vercel configuration

---
+++ index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+import { router as embedRouter } from "./src/routes/embeb";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(embedRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
